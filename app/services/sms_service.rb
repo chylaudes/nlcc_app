b1fca2e2f6bb2b6203522cf61c43b9435668f761
Christine Lau <chylau.design@gmail.com>
2016-02-17 16:41:11 -0800

    starting the scheduling

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -8,18 +8,17 @@ class SmsService
   end
 
   def send_messages(job)
-    contacts = Contacts.all
-    contacts.each do |contact|
+    # contacts = Contact.all
+    # contacts.each do |contact|
 
       account_sid = ENV['twillio_sid']
       auth_token = ENV['twillio_token']
-      @job = job
       @client = Twilio::REST::Client.new account_sid, auth_token
       @client.account.messages.create({
         :from => ENV['twillio_number'],
-        :to => contact.phone_number,
-        :body => "Courtesy Reminder: This Sunday, #{@job.sunday_date.strftime('%m/%d')}, New Life Community Church is meeting at the #{@job.hotel.name}",
+        :to => "+15105906673",
+        :body => "Courtesy Reminder: This Sunday, #{job.sunday_date.strftime('%m/%d')}, New Life Community Church is meeting at the #{job.hotel.name}, #{job.hotel.address}, Hope to see you there!",
       })
-  end
+  # end
   end
 end
